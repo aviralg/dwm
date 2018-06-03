fe8318823fe98964a1150e5073a3fa6785718fbe
Aviral Goel <goel.aviral@gmail.com>
2018-06-03 12:23:07 -0400

    Apply cyclelayouts patch
    Cycle through all the available layouts.
    
    Patch: https://dwm.suckless.org/patches/cyclelayouts/dwm-cyclelayouts-20180524-a09e766.diff
    Description: https://dwm.suckless.org/patches/cyclelayouts/

--- config.def.h
+++ config.def.h
@@ -49,6 +49,7 @@ static const Layout layouts[] = {
   { ">M>",      centeredfloatingmaster },
   { "TTT",      bstack },
   { "===",      bstackhoriz },
+  { NULL,       NULL},
 };
 
 /* key definitions */
@@ -89,6 +90,8 @@ static Key keys[] = {
   { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
   { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[7]} },
   { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[8]} },
+  { MODKEY|ControlMask,		        XK_comma,  cyclelayout,    {.i = -1 } },
+  { MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
